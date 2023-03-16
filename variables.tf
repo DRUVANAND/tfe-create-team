86875ab768b187bb2474091323ef0ba7201129b6
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-16 16:31:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "team_token" {
+variable "tfe_token" {
   type        = string
   description = "Team API token to use for Terraform Cloud operations"
   default = "Qa3YnezRzlgfZQ.atlasv1.LfsPAiy9EsIl2boZXbqp6tXeG4YR6QHVxJ3nrO5gtygIRzl7IgShgjAAaq2q74LAvCo"
