c8a9a479dd53fdb4238ae9058ccb97aec52d9085
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-16 14:34:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,8 +4,8 @@ terraform {
       version = "~> 0.42.0"
     }
   }
+}
 
   provider "tfe" {
     team_token = "Qa3YnezRzlgfZQ.atlasv1.LfsPAiy9EsIl2boZXbqp6tXeG4YR6QHVxJ3nrO5gtygIRzl7IgShgjAAaq2q74LAvCo"
   }
-}
