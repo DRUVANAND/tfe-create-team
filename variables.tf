4b86327e6a7802a80086e2bd0d82ba8cc830de62
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-16 13:35:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "team_name" {
   type    = string
   default = "dio-team"
 }
+variable "token_scopes" {
+  type        = list(string)
+  default     = ["team:*"]
+  description = "List of API token scopes"
+}
