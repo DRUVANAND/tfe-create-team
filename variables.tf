646a54908415342dee32220112f6badaf12f59fd
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-16 15:07:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "tfe-token" {
   type = string
   description = "User API token to use for Terraform Cloud operations"
-  default = "VVfhzomzjNB6oA.atlasv1.7gaZRFrljbp9ylYzGY8zhVRXCGPeDv2JQhSWVayiuZCNyV3geApMI2whw2GtoVj4Qyc"
+  value = "VVfhzomzjNB6oA.atlasv1.7gaZRFrljbp9ylYzGY8zhVRXCGPeDv2JQhSWVayiuZCNyV3geApMI2whw2GtoVj4Qyc"
 }
