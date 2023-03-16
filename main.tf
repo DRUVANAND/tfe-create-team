a37d775b476b36837389720d681b1ec3939377f1
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-16 13:55:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "tfe_team" "my_team" {
-  organization = var.org_name
-  name         = var.team_name
+  organization = "ops360systems"
+  name         = "dio-team"
   visibility   = "secret"
 }
