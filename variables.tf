ea6a7e5388b1e778b7f8abd5529a719aaac7063d
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-16 16:29:41 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "team_token" {
+  type        = string
+  description = "Team API token to use for Terraform Cloud operations"
+  default = "Qa3YnezRzlgfZQ.atlasv1.LfsPAiy9EsIl2boZXbqp6tXeG4YR6QHVxJ3nrO5gtygIRzl7IgShgjAAaq2q74LAvCo"
+}
