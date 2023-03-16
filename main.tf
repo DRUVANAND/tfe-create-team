f1408eca558407ffe4feb8df3ec4f17331dc42c0
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-16 13:31:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "tfe_team" "my_team" {
   name         = var.team_name
   visibility   = "secret"
 }
+resource "tfe_team_token" "my_team_token" {
+  team_id     = tfe_team.my_team.id
+  description = "API token for ${tfe_team.my_team.name}"
+  scopes      = var.token_scopes
+}
