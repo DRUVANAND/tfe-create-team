c973899400e413d8c401fef510645a8f72cc8680
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-16 16:05:36 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "tfe-token" {
-  type = string
-  description = "User API token to use for Terraform Cloud operations"
-  default = "Qa3YnezRzlgfZQ.atlasv1.LfsPAiy9EsIl2boZXbqp6tXeG4YR6QHVxJ3nrO5gtygIRzl7IgShgjAAaq2q74LAvCo"
-}
