2ea768e4481a701eb22291897b90c3ed20d1a98f
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-16 13:41:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
-
-resource "tfe_team_token" "my_team_token" {
-  team_id     = tfe_team.my_team.id
-  description = "API token for ${tfe_team.my_team.name}"
-  scopes      = var.token_scopes
+resource "tfe_team" "my_team" {
+  organization = var.org_name
+  name         = var.team_name
+  visibility   = "secret"
 }
